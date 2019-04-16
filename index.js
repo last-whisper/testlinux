2a9ba9522357b16d30a0a620aed67eec8f63dbed
qingyu <qingyu@enai.im>
2019-04-16 14:24:52 +0800

    port

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const http = require("http")
 http.createServer((request,response)=>{
     response.writeHead(200,{'Content-Type':'text/plain'})
     response.end('hello last-whisper')
-}).listen(8080)
+}).listen(80)
